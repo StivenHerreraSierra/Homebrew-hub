6373504a5c6172b61ccff636d1fd5242bbad6591
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2022-09-27 22:19:16 -0500

    Diseño lista de paquetes

--- homebrew-hub/src/app/app.module.ts
+++ homebrew-hub/src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Componentes
+import { ListaComponent } from './components/lista/lista.component';
+
 //Material
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { ListaItemComponent } from './components/lista-item/lista-item.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListaComponent, ListaItemComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
     //Material
     MatGridListModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatChipsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ homebrew-hub/src/app/components/lista-item/lista-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-item',
+  templateUrl: './lista-item.component.html',
+  styleUrls: ['./lista-item.component.css']
+})
+export class ListaItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
