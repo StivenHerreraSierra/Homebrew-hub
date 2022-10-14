f637e749288b35aba3cc8a86d07efb56a9329243
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2022-10-14 17:30:49 -0500

    Acceso a detalles desde la lista de paquetes

--- homebrew-hub/src/app/components/lista-item/lista-item.component.ts
+++ homebrew-hub/src/app/components/lista-item/lista-item.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Paquete } from 'src/app/models/package.model';
 
 @Component({
   selector: 'app-lista-item',
   templateUrl: './lista-item.component.html',
-  styleUrls: ['./lista-item.component.css']
+  styleUrls: ['./lista-item.component.css'],
 })
 export class ListaItemComponent implements OnInit {
   @Input() paquete!: Paquete;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  irADetalles() {
+    this.router.navigate(['detalles', this.paquete.name]);
   }
-
 }
