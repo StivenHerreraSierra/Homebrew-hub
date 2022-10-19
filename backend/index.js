f3035f40e63e516fef696929de6bba27c1b5d6bc
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2022-10-18 19:14:45 -0500

    Morgan en el backend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import rutas from './src/routes/routes.js';
+import morgan from 'morgan';
 
 // Recuperar las variables de .env
 dotenv.config();
 
 // Creación del servidor.
 const app = express();
+app.use(morgan('tiny'));
 
 // Configuración del cors.
 app.use(cors({
