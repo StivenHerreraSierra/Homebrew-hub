c36eae3d50f775c36e2c55a0b5f7c3ecfb0b5573
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2022-11-19 15:11:40 -0500

    Ajuste en la ruta a la api

--- homebrew-hub/src/environments/environment.prod.ts
+++ homebrew-hub/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@
 export const environment = {
   production: true,
   //api: 'https://homebrewhub-api.onrender.com/api',
-  api: 'https://homebrewhub-api-gsk5.onrender.com'
+  api: 'https://homebrewhub-api-gsk5.onrender.com/api'
 //api: 'http://ec2-67-202-32-193.compute-1.amazonaws.com/api'
 };
