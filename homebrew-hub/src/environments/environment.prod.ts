a1bb63ae2071a0d4e840d7762ad9504659fd029b
Stiven Herrera Sierra <60407778+StivenHerreraSierra@users.noreply.github.com>
2022-11-18 21:52:23 -0500

    Nuevo host backend

--- homebrew-hub/src/environments/environment.prod.ts
+++ homebrew-hub/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@
  */
 export const environment = {
   production: true,
-  api: 'https://homebrewhub-api.onrender.com/api',
+  //api: 'https://homebrewhub-api.onrender.com/api',
+  api: 'https://homebrewhub-api-gsk5.onrender.com'
 //api: 'http://ec2-67-202-32-193.compute-1.amazonaws.com/api'
 };
